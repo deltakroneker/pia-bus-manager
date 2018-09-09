a768df1abdbc83ec652fad5f055de83b018694af
deltakroneker <nikola.milic.ios@gmail.com>
2018-09-09 11:39:24 +0200

    Add CityLine, Drivers ctrls and more

--- src/java/ctrls/Navigation.java
+++ src/java/ctrls/Navigation.java
@@ -3,18 +3,84 @@ package ctrls;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import java.util.List;
+import java.util.logging.Logger;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import objs.User;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import utils.Constants;
+import utils.NewHibernateUtil;
 
 @Named(value = "navigationCtrl")
 @SessionScoped
 public class Navigation implements Serializable {
 
+    private String username;
+    private String password;
+    private String passwordHash;
+    
+    private String firstName;
+    private String lastName;
+    
     public Navigation() {
+    }
+    
+    public String login(){
+        FacesContext context = FacesContext.getCurrentInstance();
+
+        String address = "login";
+        
+        Session s = NewHibernateUtil.getSessionFactory().openSession();
+        Transaction tx = null;
+        
+        try {
+            tx = s.beginTransaction();        
+            List<User> list = s.createCriteria(User.class).list();
+            
+            for (User user : list) {
+                if (user.getUsername().equals(getUsername()))
+                    if (getUsername().equals(Constants.ADMIN_USERNAME)) {
+                        address = adminHome();
+                        //context.getExternalContext().getSessionMap().put("user", user);
+                        firstName = user.getFirstname();
+                        lastName = user.getLastname();
+                        break;
+                    }
+                    else {
+                        address = userHome();
+                        //context.getExternalContext().getSessionMap().put("user", user);
+                        firstName = user.getFirstname();
+                        lastName = user.getLastname();
+                        break;
+                    }
+                else 
+                    context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Username or password is not valid.", null));
+            }
+            
+            tx.commit();
+        } catch (HibernateException ex) {
+            if (tx != null) {
+                tx.rollback();
+            }            
+            Logger.getLogger("con").info("Exception: " + ex.getMessage());
+            ex.printStackTrace(System.err);
+        } finally {
+            s.close(); 
+        }
+               
+        return address;
     }
     
     public String logout(){
        // TODO: unload all login info
        return Constants.LOGIN_PAGE;
+    }
+    
+    public String register(){
+       return Constants.REGISTER_PAGE;
     }
     
     public String guestHome(){
@@ -27,5 +93,99 @@ public class Navigation implements Serializable {
     
     public String adminHome(){
        return Constants.ADMINHOME_PAGE;
+    }
+    
+    public String adminCLM(){
+        return Constants.ADMINCLM_PAGE;
+    }
+    
+    public String adminICLM(){
+        return Constants.ADMINICLM_PAGE;
+    }
+    
+    public String adminRM(){
+        return Constants.ADMINRM_PAGE;
+    }
+
+    public String adminND(){
+        return Constants.ADMINND_PAGE;
+    }
+     
+    public String adminNCL(){
+        return Constants.ADMINNCL_PAGE;
+    }
+      
+    public String adminDCL(){
+        return Constants.ADMINDCL_PAGE;
+    }
+    
+    /**
+     * @return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return the passwordHash
+     */
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    /**
+     * @param passwordHash the passwordHash to set
+     */
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * @return the lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @param lastName the lastName to set
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 }
--- src/java/utils/Constants.java
+++ src/java/utils/Constants.java
@@ -3,11 +3,22 @@ package utils;
 public class Constants {
     
     public static String LOGIN_PAGE = "/auth/login";
-    public static String REGISTER_PAGE = "/auth/login";
-    public static String GUESTHOME_PAGE = "/auth/login";
-    public static String USERHOME_PAGE = "/auth/login";
-    public static String ADMINHOME_PAGE = "/auth/login";
+    public static String REGISTER_PAGE = "/auth/register";
+    
+    public static String GUESTHOME_PAGE = "/home/guestHome";
+    public static String USERHOME_PAGE = "/home/userHome";
+    public static String ADMINHOME_PAGE = "/home/adminHome";
+    
+    public static String ADMINCLM_PAGE = "/admin/cityLineManager";
+    public static String ADMINICLM_PAGE = "/admin/interCityLineManager";
+    public static String ADMINRM_PAGE = "/admin/requestManager";
+    
+    public static String ADMINND_PAGE = "/admin/newDriver";
+    public static String ADMINNCL_PAGE = "/admin/newCityLine";
+    public static String ADMINDCL_PAGE = "/admin/disableCityLine";
+
     
     public static String SALT = "salt-bae";
+    public static String ADMIN_USERNAME = "admin";
     
 }
